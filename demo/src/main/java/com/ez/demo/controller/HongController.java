5fab3e9c5505a217d7c27fa2ef356352854d4af6
EZEN <EZEN@DESKTOP-56VTHAK>
2022-06-10 12:59:08 +0900

    컨트롤러, index 수정 2개 추가 by hong

---
+++ demo/src/main/java/com/ez/demo/controller/HongController.java
@@ -0,0 +1,8 @@
+package com.ez.demo.controller;
+
+public class HongController {
+	
+	public void hon() {
+		System.out.println("hong!");
+	}
+}
--- demo/src/main/java/com/ez/demo/controller/TestController.java
+++ demo/src/main/java/com/ez/demo/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.ez.demo.controller;
 
 public class TestController {
-
+	public String test() {
+		System.out.println("test by hong");
+		return "/index";
+	}
 }
